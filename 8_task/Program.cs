e50bb3d210101d4260f16e2170c4a1f8a20d511c
ShynarZhakan <schubblezam@gmail.com>
2022-11-24 23:07:43 +0600

    added 7 and 8 tasks

---
+++ 8_task/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+
+if (num % 2 == 1)
+{
+    num = num - 1;
+}
+
+else 
+{
+    num = num;
+}
+
+while (i <= num)
+{
+    Console.WriteLine(i);
+    i+=2;
+}
